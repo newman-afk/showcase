b9a0781cd938901b98b12b319b27c44440c82c4f
newman-afk <84593706+newman-afk@users.noreply.github.com>
2023-05-29 06:27:36 +0800

    try again

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,17 +1,17 @@
-// import Carousel from "../components/Carousel";
-// import ProductInfo from "../components/ProductInfo";
+import Carousel from "../components/Carousel";
+import ProductInfo from "../components/ProductInfo";
 
 export default function Home() {
   return (
     <div className="bg-slate-50 py-8 dark:bg-slate-800">
-      {/* <Carousel /> */}
+      <Carousel />
       <div
         className=" mt-20 text-center text-2xl font-semibold uppercase dark:text-orange-100"
         style={{ letterSpacing: "0.5vw" }}
       >
         Product description
       </div>
-      {/* <ProductInfo /> */}
+      <ProductInfo />
     </div>
   );
 }

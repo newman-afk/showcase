96b3b964a127ac8363407f28fe3e6b2873619a53
newman-afk <84593706+newman-afk@users.noreply.github.com>
2023-06-08 15:02:29 +0800

    Forget to save change

--- app/components/DataTablePagination.tsx
+++ app/components/DataTablePagination.tsx
@@ -25,8 +25,8 @@ export function DataTablePagination<TData>({
   return (
     <div className="mt-4 flex items-center justify-between px-2">
       <div className="flex-1 text-sm text-muted-foreground">
-        {table.getFilteredSelectedRowModel().rows.length} of&nbsp;
-        {table.getFilteredRowModel().rows.length} product(s) selected.
+        {table.getFilteredSelectedRowModel()?.rows?.length} of&nbsp;
+        {table.getFilteredRowModel()?.rows?.length} product(s) selected.
       </div>
       <div className="flex items-center space-x-6 lg:space-x-8">
         <div className="flex items-center space-x-2">
